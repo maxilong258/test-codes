b8744f444b9fad0906e3464ca07ce5e467a5bb64
maxilong <adc3080027554@gmail.com>
2025-09-17 11:32:08 +0800

    1

---
+++ solutions/160. 相交链表.ts
@@ -0,0 +1,21 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function getIntersectionNode(
+  headA: ListNode | null,
+  headB: ListNode | null
+): ListNode | null {
+  let p1 = headA,
+    p2 = headB;
+  while (p1 !== p2) {
+    p1 = p1 ? p1.next : headB;
+    p2 = p2 ? p2.next : headA;
+  }
+  return p1;
+}
